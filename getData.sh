71fc8bac23a9b8db0fdcf6d4b197c6973862c968
Tibor Koroknai <tkorokn@clemson.edu>
2015-10-21 10:26:52 -0400

    After update need to use transfer node!

--- getData.sh
+++ getData.sh
@@ -2,7 +2,7 @@
 time_stamp=$(date +%Y_%m_%d_%H_%M_%S)
 mkdir "${time_stamp}$1"
 cd "${time_stamp}$1"
-scp "$1"@user.palmetto.clemson.edu:/scratch1/"$1"/Root_step10/Dosimetry_Detector_* ./
+scp "$1"@xfer01-ext.palmetto.clemson.edu:/scratch1/"$1"/Root_step10/Dosimetry_Detector_* ./
 hadd Dosimetry_Detector_tot.root Dosimetry_Detector_*
 mkdir out && cd out
 scp "$1"@user.palmetto.clemson.edu:/home/"$1"/geant4_workdir/spinotron/out/* ./
