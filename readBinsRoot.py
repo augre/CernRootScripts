ef58f002b6ec16261b9077926391d5f0944eae80
Tibor Koroknai <tkorokn@clemson.edu>
2015-10-25 17:35:47 -0400

    added one function choice when run. Use root's algorithms to find max value and x for y.

--- readBinsRoot.py
+++ readBinsRoot.py
@@ -9,27 +9,73 @@ from scipy.optimize import fmin
 import sys, getopt
 from myTypes import Location, RootFile
 #import pdb
-from fitFunctions import tripleGaussian, findInverseValueTriple, voigt
+from fitFunctions import tripleGaussian, findInverseValue, voigt
+
+def printInfo(myFitFunc, axis="x"):
+    #Get maximum of fit function
+    par=myFitFunc.GetParameters()
+    fitFuncMax=myFitFunc.GetMaximum(-80,80)
+    print "y max= ", fitFuncMax
+    #get x value @ .9*ymax, .8*ymax, .5*ymax
+    x90=myFitFunc.GetX(.9*fitFuncMax,-80, 0)
+    x80=myFitFunc.GetX(.8*fitFuncMax,-80, 0)
+    x50=myFitFunc.GetX(.5*fitFuncMax,-80, 0)
+    x90plus=myFitFunc.GetX(.9*fitFuncMax,0, 80)
+    x80plus=myFitFunc.GetX(.8*fitFuncMax,0, 80)
+    x50plus=myFitFunc.GetX(.5*fitFuncMax,0, 80)
+    print "90% of max value @ x= ",x90
+    print "80% of max value @ x= ",x80
+    print "50% of max value @ x= ",x50
+    print "90% of max value @ x= ",x90plus
+    print "80% of max value @ x= ",x80plus
+    print "50% of max value @ x= ",x50plus
+    print axis,"axis distance from 90% to 50% on minus side is: ",x90-x50, "mm"
+    print axis,"axis distance from 90% to 50% on plus side is: ",x50plus-x90plus, "mm"
+    print axis,"axis distance from 90% minus to 90% plus is: ",x90plus-x90, "mm"
+    print axis,"axis distance from 80% minus to 80% plus is: ",x80plus-x80, "mm"
 
 
 def main(argv):
     inputfile = 'Dosimetry_Detector_tot.root'
     outputfile = 'outputPlots.png'
+    function=voigt
     try:
-        opts, args = getopt.getopt(argv[1:],"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv[1:],"hi:o:f:",["ifile=","ofile=","function="])
     except getopt.GetoptError:
-        print argv[0],'-i <inputfile> -o <outputfile>'
+        print argv[0],'-i <inputfile> -o <outputfile> -f <function>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print argv[0],'-i <inputfile> -o <outputfile>'
+            print argv[0],'-i <inputfile> -o <outputfile> -f <function>'
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
+        elif opt in ("-f", "--function"):
+            if arg == "voigt":
+                function = voigt
+                fname="voigt"
+                myFitFunc=r.TF1("voigt",function,-80,80, 3)
+            elif arg == "tripleGaussian":
+                function = tripleGaussian
+                fname="tripleGaussian"
+                myFitFunc=r.TF1("tripleGaussian",function,-80,80, 9)
+                myFitFunc.SetParName(0, "norm")
+                myFitFunc.SetParName(1, "mu1")
+                myFitFunc.SetParName(2, "sigma1")
+                myFitFunc.SetParName(3, "mu2")
+                myFitFunc.SetParName(4, "sigma2")
+                myFitFunc.SetParName(5, "mu3")
+                myFitFunc.SetParName(6, "sigma3")
+                myFitFunc.SetParName(7, "frac1")
+                myFitFunc.SetParName(8, "frac2")
+            else:
+                print "you can choose from voigt,tripleGaussian"
+
     print 'Input file is "', inputfile
     print 'Output file is "', outputfile
+    print 'Function is "', fname
 
     f = RootFile()
     f.loadFile()
@@ -51,113 +97,61 @@ def main(argv):
 #    pdb.set_trace()
 
    
-    maxxy=f.rootValXY.GetBinContent(f.rootValXY.GetMaximumBin())
-    max3D=f.histObj.GetBinContent(f.histObj.GetMaximumBin())
-    print "maxXY:",maxxy,"max3D",max3D
-    print maxxy
-    c2=r.TCanvas("c2")
-    r.gStyle.SetOptStat(0)
-    f.rootValXY.SetContour(10)
-    f.rootValXY.DrawCopy("colz")
-    f.rootValXY.Draw("cont3 same")
-    f.rootValXY.Draw()
-    f.rootValXY.SetLineColor(r.kRed)
-    c2.Print("test.png")
+#    maxxy=f.rootValXY.GetBinContent(f.rootValXY.GetMaximumBin())
+#    max3D=f.histObj.GetBinContent(f.histObj.GetMaximumBin())
+#    print "maxXY:",maxxy,"max3D",max3D
+#    print maxxy
+#    c2=r.TCanvas("c2")
+#    r.gStyle.SetOptStat(0)
+#    f.rootValXY.SetContour(10)
+#    f.rootValXY.DrawCopy("colz")
+#    f.rootValXY.Draw("cont3 same")
+#    f.rootValXY.Draw()
+#    f.rootValXY.SetLineColor(r.kRed)
+#    c2.Print("test.png")
 
 #    r.gStyle.SetEndErrorSize(3)
     r.gStyle.SetErrorX(0.)
     
 
-    #choose fitFunction and set up parameter names and values    
-#    myFitFunc=r.TF1("tripleGaussian",tripleGaussian,-80,80, 9)
-#    myFitFunc.SetParName(0, "norm")
-#    myFitFunc.SetParName(1, "mu1")
-#    myFitFunc.SetParName(2, "sigma1")
-#    myFitFunc.SetParName(3, "mu2")
-#    myFitFunc.SetParName(4, "sigma2")
-#    myFitFunc.SetParName(5, "mu3")
-#    myFitFunc.SetParName(6, "sigma3")
-#    myFitFunc.SetParName(7, "frac1")
-#    myFitFunc.SetParName(8, "frac2")
-
-#    myFitFunc.SetParameters(1500, 5, .5, 6, 1.5, 5, 5, .4, .3)
-
-    myFitFunc=r.TF1("voigt",voigt,-80,80, 3)
-    myFitFunc.SetParameters(500, 5, .5, )
     c1=r.TCanvas("c1", "", 1000,1000)
     c1.Divide(1,3)
 
     c1.cd(1)
-    f.rootValX.Fit("voigt")
+    if function==tripleGaussian:
+        myFitFunc.SetParameters(1500, 5, .5, 6, 1.5, 5, 5, .4, .3)
+    elif function==voigt:
+        myFitFunc.SetParameters(500, 5, .5, )
+    f.rootValX.Fit(fname)
     f.rootValX.SetMarkerStyle(20)
     f.rootValX.Draw("E1")
 
-#    #Get maximum of fit function
-#    par=myFitFunc.GetParameters()
-#    fitFuncMax=fmin(lambda x: -tripleGaussian(x, par), -80)
-#    print fitFuncMax
-#    #get x value @ .9*y and .5*y
-#    x90=findInverseValueTriple(.9*tripleGaussian(fitFuncMax,par), par,-80, 80 )
-#    x50=findInverseValueTriple(.5*tripleGaussian(fitFuncMax,par), par,-80, 80 )
-#    x90plus=findInverseValueTriple(.9*tripleGaussian(fitFuncMax,par), par,80, -80, -1)
-#    x50plus=findInverseValueTriple(.5*tripleGaussian(fitFuncMax,par), par,80, -80, -1)
-#    print "90% of y max @ x= ",x90
-#    print "50% of y max @ x= ",x50
-#    print "90% of y max @ x= ",x90plus
-#    print "50% of y max @ x= ",x50plus
-#    print "x axis distance from 90% to 50% on minus side is: ",x90[0]-x50[0], "mm"
-#    print "x axis distance from 90% to 50% on plus side is: ",x50plus[0]-x90plus[0], "mm"
-#    print "x axis distance from 90% minus to 90% plus is: ",x90plus[0]-x90[0], "mm"
+    printInfo(myFitFunc, "x")
 
     c1.cd(2)
-    myFitFunc.SetParameters(500, 5, .5, )
-    f.rootValY.Fit("voigt")
+    if function==tripleGaussian:
+        myFitFunc.SetParameters(1500, 5, .5, 6, 1.5, 5, 5, .4, .3)
+    elif function==voigt:
+        myFitFunc.SetParameters(500, 5, .5, )
+    f.rootValY.Fit(fname)
     f.rootValY.SetMarkerStyle(20)
     f.rootValY.Draw("E1")
 
-#    #Get maximum of fit function
-#    par=myFitFunc.GetParameters()
-#    fitFuncMax=fmin(lambda x: -tripleGaussian(x, par), -80)
-#    print fitFuncMax
-#    #get x value @ .9*y and .5*y
-#    x90=findInverseValueTriple(.9*tripleGaussian(fitFuncMax,par), par,-80, 80 )
-#    x50=findInverseValueTriple(.5*tripleGaussian(fitFuncMax,par), par,-80, 80 )
-#    x90plus=findInverseValueTriple(.9*tripleGaussian(fitFuncMax,par), par,80, -80, -1)
-#    x50plus=findInverseValueTriple(.5*tripleGaussian(fitFuncMax,par), par,80, -80, -1)
-#    print "90% of y max @ x= ",x90
-#    print "50% of y max @ x= ",x50
-#    print "90% of y max @ x= ",x90plus
-#    print "50% of y max @ x= ",x50plus
-#    print "y axis distance from 90% to 50% is: ",x90[0]-x50[0], "mm"
-#    print "y axis distance from 90% to 50% on plus side is: ",x50plus[0]-x90plus[0], "mm"
-#    print "y axis distance from 90% minus to 90% plus is: ",x90plus[0]-x90[0], "mm"
-
+    printInfo(myFitFunc,"y")
 
     c1.cd(3)
-    myFitFunc.SetParameters(500, 5, .5, )
-    f.rootValZ.Fit("voigt")
+    if function==tripleGaussian:
+        myFitFunc.SetParameters(1500, 5, .5, 6, 1.5, 5, 5, .4, .3)
+    elif function==voigt:
+        myFitFunc.SetParameters(500, 5, .5, )
+    f.rootValZ.Fit(fname)
     f.rootValZ.SetMarkerStyle(20)
     f.rootValZ.Draw("E1")
 
-#    #Get maximum of fit function
-#    par=myFitFunc.GetParameters()
-#    fitFuncMax=fmin(lambda x: -tripleGaussian(x, par), -80)
-#    print fitFuncMax
-#    #get x value @ .9*y and .5*y
-#    x90=findInverseValueTriple(.9*tripleGaussian(fitFuncMax,par), par,-80, 80 )
-#    x50=findInverseValueTriple(.5*tripleGaussian(fitFuncMax,par), par,-80, 80 )
-#    x90plus=findInverseValueTriple(.9*tripleGaussian(fitFuncMax,par), par,80, -80, -1)
-#    x50plus=findInverseValueTriple(.5*tripleGaussian(fitFuncMax,par), par,80, -80, -1)
-#    print "90% of y max @ x= ",x90
-#    print "50% of y max @ x= ",x50
-#    print "90% of y max @ x= ",x90plus
-#    print "50% of y max @ x= ",x50plus
-#    print "z axis distance from 90% to 50% is: ",x90[0]-x50[0], "mm"
-#    print "z axis distance from 90% to 50% on plus side is: ",x50plus[0]-x90plus[0], "mm"
-#    print "z axis distance from 90% minus to 90% plus is: ",x90plus[0]-x90[0], "mm"
-
+    printInfo(myFitFunc,"z")
 
     c1.Print(outputfile)
+
 
     
 if __name__ == "__main__":
