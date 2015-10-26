123beb91a357c5a04486709e89f4f0996030c56b
Tibor Koroknai <tkorokn@clemson.edu>
2015-10-25 20:09:28 -0400

    added zip command. Don't use yet!

--- tkprocess.sh
+++ tkprocess.sh
@@ -1,2 +1,3 @@
 find . -type d -exec sh -c '(cd {} && tkmain.py -f voigt -o voigt.png > voigt.txt)' ';'
 find . -type d -exec sh -c '(cd {} && tkmain.py -f tripleGaussian -o gaus.png > gaus.txt)' ';'
+find . -maxdepth 2 -type f \( -name "voigt.*" -or -name "gaus.*" \) -print | zip "${PWD##*/}" -@
